a23181bd69145ea26f78b19d176f4bbe4abc6566
chdonncha <chdonncha@gmail.com>
2016-03-28 23:52:51 +0000

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+##require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
@@ -7,12 +7,12 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should be valid" do
+   test "should be valid" do
     assert @user.valid?
   end
 
   test "name should be present" do
-    @user.name = ""
+    @user.name = "     "
     assert_not @user.valid?
   end
 
@@ -39,17 +39,19 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
    end
-    
-     test "email addresses should be unique" do
+   
+    test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
-  
-   test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should have a minimum length" do
